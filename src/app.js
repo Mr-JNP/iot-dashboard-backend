607f5d05334a90fe0183e18b7d6a47e9ce7abfdd
Nontapat Pintira <nontapat.pin@gmail.com>
2020-12-25 21:12:08 +0700

    Mongoose

--- src/app.js
+++ src/app.js
@@ -1,19 +1,49 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const socket = require("socket.io");
+
 const mockdata = require("./utils/mockdata");
-const websocket = require("./websocket");
+const MQTT = require("./model");
 
 const app = express();
+
+mongoose
+  .connect("mongodb://192.168.137.40:27017/test", {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error(err, "Unable to connect to MongoDB"));
+
 const port = 3100;
 
 app.use(cors());
-
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.status(200).json(mockdata);
 });
 
 const server = require("http").createServer(app);
-websocket(server);
+const io = socket(server);
+
+io.on("connection", () => {
+  console.info(`client ${socket.id} connected`);
+});
+
+setInterval(async () => {
+  const [temperature, humidity] = await MQTT.find()
+    .sort({ timestamp: -1 })
+    .limit(2);
+
+  const dht = {
+    time: temperature.datetime,
+    temperature: temperature.payload,
+    humidity: humidity.payload,
+  };
+
+  const topic = "dht";
+  console.info(`DHT : ${JSON.stringify(dht)}`);
+  io.emit(topic, dht);
+}, 10000);
 
 server.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ src/model/index.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const MQTT = mongoose.model(
+  "MQTT",
+  new mongoose.Schema({
+    _id: { type: mongoose.Schema.Types.ObjectId },
+    topic: { type: Number },
+    payload: { type: Number },
+    qos: { type: Number },
+    timestamp: { type: Number },
+    datetime: { type: String },
+  }),
+  "mqtt"
+);
+
+module.exports = MQTT;
