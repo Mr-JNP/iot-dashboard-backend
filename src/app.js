a741fcf18d8076babea642ca88f1612807783bbc
Nontapat Pintira <nontapat.pin@gmail.com>
2020-12-25 13:32:34 +0700

    Websocket

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mockdata = require("./utils/mockdata");
+const websocket = require("./websocket");
 
 const app = express();
 const port = 3100;
@@ -11,6 +12,9 @@ app.get("/", (req, res) => {
   res.status(200).json(mockdata);
 });
 
-app.listen(port, () => {
+const server = require("http").createServer(app);
+websocket(server);
+
+server.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
