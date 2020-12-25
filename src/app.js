bb1e06d040aec9e7aa78ba8936dbde51ba35445c
Nontapat Pintira <nontapat.pin@gmail.com>
2020-12-25 11:09:51 +0700

    Configure CORS

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const mockdata = require("./utils/mockdata");
 
 const app = express();
 const port = 3100;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200).json(mockdata);
